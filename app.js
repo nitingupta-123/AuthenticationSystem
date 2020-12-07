4d1ca0771b0bf98fadcca10b7d8c6b9698b82bb7
nitingupta-123 <nitingupta03012000@gmail.com>
2020-12-07 18:42:51 +0530

    first commit to deploy on heroku

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const  { requireAuth ,checkUser} = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(8000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth,(req, res) => res.render('smoothies'));
+app.use(authRoutes);

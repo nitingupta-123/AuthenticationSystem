1b59bbbd13ff782c96baefd549c0d616341db7ca
nitingupta-123 <nitingupta03012000@gmail.com>
2020-12-07 18:58:53 +0530

    modifiying port env file and package.json file for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const  { requireAuth ,checkUser} = require('./middleware/authMiddleware');
-
+const port =process.env.PORT||8000;
 const app = express();
 
 // middleware
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 // database connection
 
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(8000))
+  .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
 // routes
